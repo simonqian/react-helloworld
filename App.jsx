a5eb20a62b2388a5a96070da2d1e16e09cb0669d
tnirupama3 <62882088+tnirupama3@users.noreply.github.com>
2023-04-11 17:59:54 -0700

    Inputting height and weight

--- App.jsx
+++ App.jsx
@@ -1,11 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
+import 
 
-class App extends React.Component {
-  render() {
-    return (
-      <div>simon, helloworld!!!</div>
-    );
+
+const App = () => {
+  const [height, setHeight] = useState('');
+  const [weight, setWeight] = useState('');
+  const [bmi, setBMI] = useState('');
+
+  const handleHeightChange = event => {
+    setHeight(event.target.value);
   }
+
+  const handleWeightChange = event => {
+    setWeight(event.target.value);
+  }
+
+  const handleCalculateBMI = () => {
+    const bmi = calculateBMI(height, weight);
+    setBMI(bmi);
+  }
+
+  return (
+    <div>
+      <label htmlFor="height">Enter your height (in cm):</label>
+      <input type="number" id="height" name="height" value={height} onChange={handleHeightChange} />
+      <br />
+      <label htmlFor="weight">Enter your weight (in kg):</label>
+      <input type="number" id="weight" name="weight" value={weight} onChange={handleWeightChange} />
+      <br />
+      <button onClick={handleCalculateBMI}>Calculate BMI</button>
+      {bmi && <p>Your BMI is {bmi}</p>}
+    </div>
+  );
 }
 
 export default App;
